ab54c09cc7aa512a9329a4da365b634201bc5670
Connie Standish <conniestand@gmail.com>
2021-04-04 17:40:54 -0400

    Added new files to main repo.

---
+++ js/main.js
@@ -0,0 +1,6 @@
+    $("#container").backstretch( [
+      "images/bridgerton-netflix.jpg",
+      "images/daphne.jpg",
+      "images/queen.jpg",
+      "images/simon-daphne-two.jpg"
+  ], {duration: 2000, transition: "fade", transitionDuration: 2000, animateFirst: false});
