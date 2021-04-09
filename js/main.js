8b195fce7f627927460ece2a7c5093067ebcd0da
Renesha Matthews <reneshamatthews9@gmail.com>
2021-04-08 20:44:23 -0400

    added smooth scrolling and back to top

--- js/main.js
+++ js/main.js
@@ -4,3 +4,54 @@
       "images/queen.jpg",
       "images/simon-daphne-two.jpg"
   ], {duration: 2000, transition: "fade", transitionDuration: 2000, animateFirst: false});
+
+
+  // Smooth Scroll
+    // Select all links with hashes
+  $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+        &&
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000, function() {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            };
+          });
+        }
+      }
+    });
+
+  var scrollToTopBtn = document.getElementById("scroll-to-top")
+  var rootElement = document.documentElement
+
+  function scrollToTop() {
+    // Scroll to top logic
+    rootElement.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    })
+  }
+  scrollToTopBtn.addEventListener("click", scrollToTop)
